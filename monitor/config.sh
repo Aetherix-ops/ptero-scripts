00bc2da88919dd6a76f0f7f1282473fcfd879c83
Aetherix <devora.support@gmail.com>
2026-06-06 17:11:30 +0800

    config.sh

---
+++ monitor/config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# =============================================================
+#  pterodactyl-scripts — monitor/config.sh
+# =============================================================
+
+# Path to Pterodactyl server volumes
+PTERODACTYL_DIR="/var/lib/pterodactyl/volumes"
